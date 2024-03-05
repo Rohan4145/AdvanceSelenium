d0462cc199413c04acad100752cabb4c7137d896
Rohan4145 <131246136+Rohan4145@users.noreply.github.com>
2024-03-05 11:03:00 +0530

    Add files via upload

---
+++ PropertyFileUtil.java
@@ -0,0 +1,19 @@
+package CommonUtils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertyFileUtil {
+
+	public String getDataFromPropertyFile (String keyname) throws IOException
+	{
+		
+		FileInputStream fis = new FileInputStream("src\\test\\resources\\Organization.xlsx");
+		Properties p = new Properties();
+		p.load(fis);
+		String value = p.getProperty(keyname);
+		return value;
+	}
+
+}
